c77ffbb6e247d593208f4fa2c0b7738ffe9a3423
Tom Lorentsen <toml@giffgaff.co.uk>
2018-05-11 18:59:22 +0100

    write graph

--- main.go
+++ main.go
@@ -4,15 +4,25 @@ import "flag"
 import (
 	"github.com/artyom/autoflags"
 	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 )
 
 var config = struct {
-	Param string `flag:"param,a parameter"`
+	Param string `flag:"repo,a parameter"`
 }{}
+
+type Graph struct {
+	Repo string `yaml:"repo"`
+}
 
 func main() {
 	autoflags.Define(&config)
 	flag.Parse()
 
-	fmt.Printf("%s\n", config.Param)
+	g := new(Graph)
+	g.Repo = config.Param
+	y, _ := yaml.Marshal(&g)
+	ioutil.WriteFile("graph.yml", y, 0644)
+	fmt.Printf("Written graph.yml:\n---\n%s---\n", y)
 }
